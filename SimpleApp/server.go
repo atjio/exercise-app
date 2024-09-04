fd81174668c9d7289e0402bdc7e951bbb098b346
Atjio <black95viper@hotmail.co.id>
2024-09-04 23:02:41 +0700

    Create simple app

---
+++ SimpleApp/server.go
@@ -0,0 +1,29 @@
+package main 
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main () {
+	portNumber := flag.String("port", "1234", "Service port")
+	flag.Parse()
+
+	port := fmt.Sprintf(":%s", *portNumber)
+
+	e := echo.New()
+	e.POST("/echo", func (c echo.Context) error {
+		bodyBytes, err := io.ReadAll(c.Request().Body)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+		return c.String(http.StatusOK, string(bodyBytes))
+	})
+
+
+	e.Logger.Fatal(e.Start(port))
+}
